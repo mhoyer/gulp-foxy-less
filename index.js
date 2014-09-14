2becdb5bf2ca5fc68a5fe052a8beb9311368a0a2
Marcel Hoyer <mhoyer@pixelplastic.de>
2014-09-14 13:06:42 +0100

    implements initial functionality and adds some test cases

--- index.js
+++ index.js
@@ -1,10 +1,69 @@
 'use strict';
 
 var through = require('through2');
+var fs = require('fs');
+var gutil = require('gulp-util');
+var path = require('path');
+var vinyl = require('vinyl');
 
 module.exports = function() {
+  var lessDependencies = {};
+  var lessDependenciesInverted = {};
+
+  function updateDependencies(file) {
+    gutil.log('Updating import dependencies for:', gutil.colors.cyan(file.path));
+
+    var filePath = path.normalize(file.path);
+
+    var data = fs.readFileSync(filePath, 'utf8');
+    var importRegex = /@import\s+["']([^"']*)["'];/g;
+
+    var matches = [];
+    var match;
+    while(match = importRegex.exec(data)) {
+      var dep = path.normalize(path.dirname(filePath) + '/' + match[1]);
+      matches.push(dep);
+
+      if (!(dep in lessDependenciesInverted)) {
+        lessDependenciesInverted[dep] = [];
+      }
+
+      if (!(filePath in lessDependenciesInverted[dep])) {
+        lessDependenciesInverted[dep].push(filePath);
+      }
+    }
+
+    lessDependencies[filePath] = matches;
+  }
+
+  var pushDependentFilesToStream = function(sourceFile, stream, pushed) {
+    pushed = pushed || [sourceFile];
+
+    if(sourceFile in lessDependenciesInverted) {
+      lessDependenciesInverted[sourceFile].forEach(function(dep) {
+        if(pushed.indexOf(dep) >= 0) return;
+
+        gutil.log('Pushing dependent file to stream:', gutil.colors.cyan(dep));
+
+        stream.push(new vinyl({path: dep}));
+        pushed.push(dep);
+
+        pushDependentFilesToStream(dep, stream, pushed);
+      });
+    }
+  };
+
   var transformFn = function (file, enc, cb) {
+    var filePath = path.normalize(file.path);
+
     this.push(file);
+
+    if(filePath in lessDependencies) {
+      pushDependentFilesToStream(filePath, this);
+    } else {
+      updateDependencies(file);
+    }
+
     cb();
   };
 
--- test/gulp-autoless-filter.spec.js
+++ test/gulp-autoless-filter.spec.js
@@ -2,30 +2,244 @@
 
 var should = require('chai').should(),
     through = require('through2'),
-    autolessFilter = require('..');
+    fs = require('fs'),
+    path = require('path'),
+    sinon = require('sinon'),
+    gulpAutolessFilter = require('..');
 
-describe('gulp-autoless-filter', function(){
+describe('Running gulp-autoless-filter task', function(){
+  var sut, buffer;
 
-  it('should pass input file to output stream', function(done) {
-    // arrange
-    var fileFixture = {path: 'any-data'};
-    var sut = autolessFilter();
+  beforeEach(function() {
+    buffer = [];
 
-    // act
-    sut.write(fileFixture);
-
-    // assert
-    var buffer = [];
-    sut.on('data', function(data) {
+    sut = gulpAutolessFilter();
+    sut.on('data', function(data){ 
       buffer.push(data);
-    }).on('end', function() {
-      buffer.length.should.equal(1);
-      buffer[0].should.equal(fileFixture);
-      done();
     });
-    
-    // cleanup
-    sut.end();
   });
 
+  afterEach(function() {  });
+
+  describe('with simple standalone .less file', function() {
+    var fileFixture = {path: __dirname+'/fixtures/standalone.less'};
+
+    it('should pass when pushed once to input stream', function(done) {
+      // act
+      sut.write(fileFixture);
+
+      // assert 
+      sut.on('end', function() {
+        buffer.length.should.equal(1);
+        buffer[0].should.equal(fileFixture);
+        done();
+      });
+      sut.end();
+    });
+
+    it('should pass twice when pushed twice to input stream', function(done) {
+      // act
+      sut.write(fileFixture);
+      sut.write(fileFixture);
+
+      // assert 
+      sut.on('end', function() {
+        buffer.length.should.equal(2);
+        buffer[0].should.equal(fileFixture);
+        buffer[1].should.equal(fileFixture);
+        done();
+      });
+      sut.end();
+    });
+  });
+
+  describe('with simple A <- B dependent .less files', function() {
+    // B.less imports A.less
+    var fileFixture = [
+      {path: path.normalize(__dirname+'/fixtures/simple-A.less')},
+      {path: path.normalize(__dirname+'/fixtures/simple-B.less')}
+    ];
+
+    it('should map from input sequence A-B to A-B', function(done) {
+      // act
+      sut.write(fileFixture[0]);
+      sut.write(fileFixture[1]);
+
+      // assert 
+      sut.on('end', function() {
+        buffer.length.should.equal(2);
+        buffer[0].should.equal(fileFixture[0]);
+        buffer[1].should.equal(fileFixture[1]);
+        done();
+      });
+      sut.end();
+    });
+
+    it('should map from input sequence B-A to B-A', function(done) {
+      // act
+      sut.write(fileFixture[1]);
+      sut.write(fileFixture[0]);
+
+      // assert 
+      sut.on('end', function() {
+        buffer.length.should.equal(2);
+        buffer[0].should.equal(fileFixture[1]);
+        buffer[1].should.equal(fileFixture[0]);
+        done();
+      });
+      sut.end();
+    });
+
+    it('should map from input sequence A-B-B to A-B-B', function(done) {
+      // act
+      sut.write(fileFixture[0]);
+      sut.write(fileFixture[1]);
+      sut.write(fileFixture[1]);
+
+      // assert 
+      sut.on('end', function() {
+        buffer.length.should.equal(3);
+        buffer[0].should.equal(fileFixture[0]);
+        buffer[1].should.equal(fileFixture[1]);
+        buffer[2].should.equal(fileFixture[1]);
+        done();
+      });
+      sut.end();
+    });
+
+    it('should map from input sequence A-B-A to A-B-A-B', function(done) {
+      // act
+      sut.write(fileFixture[0]);
+      sut.write(fileFixture[1]);
+      sut.write(fileFixture[0]);
+
+      // assert 
+      sut.on('end', function() {
+        buffer.length.should.equal(4);
+        buffer[0].should.equal(fileFixture[0]);
+        buffer[1].should.equal(fileFixture[1]);
+        buffer[2].path.should.equal(fileFixture[0].path);
+        buffer[3].path.should.equal(fileFixture[1].path);
+        done();
+      });
+      sut.end();
+    });
+  });
+
+  describe('with complex scenario: (A <- B <- C <- D) and (A <- D)', function() {
+    // A <- B <- C <─┐
+    // ^             D
+    // └─────────────┘
+    var fileFixture = [
+      {path: path.normalize(__dirname+'/fixtures/complex-A.less')},
+      {path: path.normalize(__dirname+'/fixtures/complex-B.less')},
+      {path: path.normalize(__dirname+'/fixtures/complex-C.less')},
+      {path: path.normalize(__dirname+'/fixtures/complex-D.less')}
+    ];
+
+    it('should map from input sequence A-B-C-D to A-B-C-D', function(done) {
+      // act
+      sut.write(fileFixture[0]);
+      sut.write(fileFixture[1]);
+      sut.write(fileFixture[2]);
+      sut.write(fileFixture[3]);
+
+      // assert 
+      sut.on('end', function() {
+        buffer.length.should.equal(4);
+        buffer[0].should.equal(fileFixture[0]);
+        buffer[1].should.equal(fileFixture[1]);
+        buffer[2].should.equal(fileFixture[2]);
+        buffer[3].should.equal(fileFixture[3]);
+        done();
+      });
+      sut.end();
+    });
+
+    it('should map from input sequence D-C-B-A to D-C-B-A', function(done) {
+      // act
+      sut.write(fileFixture[3]);
+      sut.write(fileFixture[2]);
+      sut.write(fileFixture[1]);
+      sut.write(fileFixture[0]);
+
+      // assert 
+      sut.on('end', function() {
+        buffer.length.should.equal(4);
+        buffer[0].should.equal(fileFixture[3]);
+        buffer[1].should.equal(fileFixture[2]);
+        buffer[2].should.equal(fileFixture[1]);
+        buffer[3].should.equal(fileFixture[0]);
+        done();
+      });
+      sut.end();
+    });
+
+    it('should map from input sequence A-B-C-D-A to A-B-C-D-A-B-C-D', function(done) {
+      // act
+      sut.write(fileFixture[0]);
+      sut.write(fileFixture[1]);
+      sut.write(fileFixture[2]);
+      sut.write(fileFixture[3]);
+      sut.write(fileFixture[0]);
+
+      // assert 
+      sut.on('end', function() {
+        buffer.length.should.equal(8);
+        buffer[0].should.equal(fileFixture[0]);
+        buffer[1].should.equal(fileFixture[1]);
+        buffer[2].should.equal(fileFixture[2]);
+        buffer[3].should.equal(fileFixture[3]);
+        buffer[4].path.should.equal(fileFixture[0].path);
+        buffer[5].path.should.equal(fileFixture[1].path);
+        buffer[6].path.should.equal(fileFixture[2].path);
+        buffer[7].path.should.equal(fileFixture[3].path);
+        done();
+      });
+      sut.end();
+    });
+
+    it('should map from input sequence A-B-C-D-B to A-B-C-D-B-C-D', function(done) {
+      // act
+      sut.write(fileFixture[0]);
+      sut.write(fileFixture[1]);
+      sut.write(fileFixture[2]);
+      sut.write(fileFixture[3]);
+      sut.write(fileFixture[1]);
+
+      // assert 
+      sut.on('end', function() {
+        buffer.length.should.equal(7);
+        buffer[0].should.equal(fileFixture[0]);
+        buffer[1].should.equal(fileFixture[1]);
+        buffer[2].should.equal(fileFixture[2]);
+        buffer[3].should.equal(fileFixture[3]);
+        buffer[4].path.should.equal(fileFixture[1].path);
+        buffer[5].path.should.equal(fileFixture[2].path);
+        buffer[6].path.should.equal(fileFixture[3].path);
+        done();
+      });
+      sut.end();
+    });
+
+    it('should map from input sequence C-D-A-C to C-D-A-C-D', function(done) {
+      // act
+      sut.write(fileFixture[2]);
+      sut.write(fileFixture[3]);
+      sut.write(fileFixture[0]);
+      sut.write(fileFixture[2]);
+
+      // assert 
+      sut.on('end', function() {
+        buffer.length.should.equal(5);
+        buffer[0].should.equal(fileFixture[2]);
+        buffer[1].should.equal(fileFixture[3]);
+        buffer[2].should.equal(fileFixture[0]);
+        buffer[3].path.should.equal(fileFixture[2].path);
+        buffer[4].path.should.equal(fileFixture[3].path);
+        done();
+      });
+      sut.end();
+    });
+  });
 });
